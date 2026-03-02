c4cb6098955bb33f13a8c3b4d341c01ce00ae7d5
SUGANTH-V-27 <suganthv87@gmail.com>
2026-03-02 13:37:47 +0530

    change from suganth

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,5 +2,6 @@
 // Using require since TypeScript compiles to CommonJS
 const app = require('../dist/server.js').default;
 
+
 // Export for Vercel - Vercel automatically handles Express apps
 module.exports = app;
